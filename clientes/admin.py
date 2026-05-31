f5458115c8555bc25a4982d4931331fcf5d089ff
Carlos Barth <carlosbarth.dev@gmail.com>
2026-05-31 17:06:51 -0300

    Melhora admin de clientes

--- clientes/admin.py
+++ clientes/admin.py
@@ -1,3 +1,25 @@
+from django import forms
 from django.contrib import admin
 
-# Register your models here.
+from .models import Cliente
+
+class ClienteAdminForm(forms.ModelForm):
+    class Meta:
+        model = Cliente
+        fields = '__all__'
+        labels = {
+            'documento': 'CPF/CNPJ',
+        }
+        widgets = {
+            'documento': forms.TextInput(attrs={
+                'placeholder': 'Somente numeros',
+            }),
+        }
+
+
+@admin.register(Cliente)
+class ClienteAdmin(admin.ModelAdmin):
+    form = ClienteAdminForm
+    list_display = ('nome', 'tipo_pessoa', 'documento', 'telefone', 'ativo')
+    list_filter = ('tipo_pessoa', 'ativo')
+    search_fields = ('nome', 'documento', 'email')
